243891aea166e734f2e8da41fc26d5d0b82047b3
Victor Shilin <chrono.monochrome@gmail.com>
2021-08-04 20:03:51 +0300

    Factor out explorer unit code

--- src/units/atomic.ts
+++ src/units/atomic.ts
@@ -1,4 +1,5 @@
 import { Api } from 'chessground/api';
+import { Chessground }  from 'chessground';
 import * as util from 'chessground/util';
 import * as cg from 'chessground/types';
 import { mapToObj } from '../util'
@@ -6,55 +7,61 @@ import { mapToObj } from '../util'
 var movesCount = 0;
 var pieces;
 
-async function getDests(fen: any): Promise<Map<any, any[]>> {
-	var enc_fen = encodeURIComponent(fen);
-	var resp = await fetch(window.location.origin + "/moves?fen=" + enc_fen, {
-	  // mode: 'no-cors',
-	  method: 'GET',
-	  headers: {
-		Accept: 'application/json',
-	  },
-	});
-	
-	var moves = await resp.text();
-	return new Map(JSON.parse(moves));
+//var DEFAULT_POSITION = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+
+declare global {
+    interface Window { chessground: any; }
 }
 
-export function move(cG: any, orig?: any, dest?: any) {	
-	if (orig != undefined && dest != undefined) {
-		console.log("Moved from " + orig + " to " + dest);
-		console.log(pieces[dest]);
-		
-		if (pieces[dest] != undefined) {
-			console.log("Destination square is occupied, calling a nuke on that square!");
-			capture(cG, dest);
-		}
-	}
-	
-	var fen = JSON.stringify(cG.getFen()).slice(1, -1) + " " + ((movesCount % 2 == 0) ? "w" : "b");
-	var turnColor = (movesCount % 2 == 0) ? "white" : "black";
+async function getDests(fen: any): Promise<Map<any, any[]>> {
+    var enc_fen = encodeURIComponent(fen);
+    var resp = await fetch(window.location.origin + "/moves?fen=" + enc_fen, {
+      // mode: 'no-cors',
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+    
+    var moves = await resp.text();
+    return new Map(JSON.parse(moves));
+}
 
-	console.log("Fen is: ");
-	console.log(fen);
-	
-	getDests(fen).then(result => {
-		console.log("Available moves: ");
-		console.log(result);
-		
-		pieces = mapToObj(cG.state.pieces);
-		console.log("Pieces on the board:");
-		console.log(pieces);
+export function move(cG: any, orig?: any, dest?: any) {    
+    if (orig != undefined && dest != undefined) {
+        console.log("Moved from " + orig + " to " + dest);
+        console.log(pieces[dest]);
+        
+        if (pieces[dest] != undefined) {
+            console.log("Destination square is occupied, calling a nuke on that square!");
+            capture(cG, dest);
+        }
+    }
+    
+    var fen = JSON.stringify(cG.getFen()).slice(1, -1) + " " + ((movesCount % 2 == 0) ? "w" : "b");
+    var turnColor = (movesCount % 2 == 0) ? "white" : "black";
 
-		cG.set({
-		  turnColor: turnColor,
-		  movable: {
-			color: turnColor,
-			dests: result
-		  }
-		});
-		
-		movesCount++;
-	});
+    console.log("Fen is: ");
+    console.log(fen);
+    
+    getDests(fen).then(result => {
+        console.log("Available moves: ");
+        console.log(result);
+        
+        pieces = mapToObj(cG.state.pieces);
+        console.log("Pieces on the board:");
+        console.log(pieces);
+
+        cG.set({
+          turnColor: turnColor,
+          movable: {
+            color: turnColor,
+            dests: result
+          }
+        });
+        
+        movesCount++;
+    });
 }
 
 export function capture(cG: Api, key: cg.Key) {
@@ -81,6 +88,32 @@ export function capture(cG: Api, key: cg.Key) {
 
 export function playOtherSide(cG: any) {
   return (orig, dest) => {
-	move(cG, orig, dest);
+    move(cG, orig, dest);
   };
+}
+
+export function getChessground(el: any, config?: any) {
+    const cG = Chessground(el, {
+      movable: {
+        color: 'white',
+        free: false,
+      },
+      draggable: {
+        showGhost: true
+      }
+    });
+
+    move(cG);
+    
+    if (config != undefined) {
+        cG.set(config);
+    }
+
+    cG.set({
+      movable: { events: { after: playOtherSide(cG) } }
+    });
+    
+    window.chessground = cG;
+
+    return cG;
 }
--- src/units/explorer.ts
+++ src/units/explorer.ts
@@ -1,26 +1,9 @@
-import { Chessground }  from 'chessground';
 import { Unit } from './unit';
-import { playOtherSide, move } from './atomic';
+import { getChessground } from './atomic';
 
 export const explorer: Unit = {
   name: 'Atomic chess explorer',
   run(el) {
-    const cG = Chessground(el, {
-      movable: {
-        color: 'white',
-        free: false,
-      },
-      draggable: {
-        showGhost: true
-      }
-    });
-
-	move(cG);
-
-    cG.set({
-      movable: { events: { after: playOtherSide(cG) } }
-    });
-
-    return cG;
+    return getChessground(el);
   }
 };
